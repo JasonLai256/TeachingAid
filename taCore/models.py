9c03912c96f6d252f2457114137424d12f4803f4
Jason Lai <jasonlai256@gmail.com>
2012-03-21 21:59:12 +0800

    fix some conflits in data model and create the database table of model.

--- taCore/models.py
+++ taCore/models.py
@@ -1,30 +1,15 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from django.contrib.auth.models import User
 
-from taggit.managers import TaggableManager
 
-class Classroom(models.Model):
-    name = models.CharField(max_length=50)
-    capacity = models.PositiveSmallIntegerField(default=0)
-    begintime = models.TimeField()  # time the classroom begin to use
-    endtime = models.TimeField()
-    status = models.CharField(max_length=30)  # 标识属于一天中的第几节课
-    courses = ManyToManyField(Course)
+class Degree(models.Model):
+    name = models.CharField(max_length=40)
 
     def __unicode__(self):
-        return "Classroom: ".format(self.name)
+        return "Degree: ".format(self.name)
 
-class Course(models.Model):
-    name = models.CharField(max_length=80)
-    teacher = models.ForeignKey(Teacher)
-    students = models.ManyToManyField(Student)
-    classes = models.ManyToManyField(Class)
-    speciality = models.ForeignKey(Speciality)
-    college = models.ForeignKey(College)
-
-    def __unicode__(self):
-        return "Course: ".format(self.name)
-    
 class College(models.Model):
     name = models.CharField(max_length=60)
 
@@ -40,16 +25,20 @@ class Speciality(models.Model):
 
 class Class(models.Model):
     name = models.CharField(max_length=60)
-    chief_teacher = models.ForeignKey(Teacher)
 
     def __unicode__(self):
         return "Class: ".format(self.name)
 
-class Degree(models.Model):
-    name = models.CharField(max_length=40)
+class Teacher(models.Model):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=50)
+    college = models.ForeignKey(College)
+    descript = models.CharField(max_length=1000)
+    leading_class = models.ForeignKey(Class)
 
     def __unicode__(self):
-        return "Degree: ".format(self.name)
+        return "teacher - ", self.user
+
 
 class Student(models.Model):
     user = models.ForeignKey(User)
@@ -61,12 +50,27 @@ class Student(models.Model):
 
     def __unicode__(self):
         return "Student - ", self.user
-    
-class Teacher(models.Model):
-    user = models.ForeignKey(User)
-    title = models.CharField(max_length=50)
+
+class Course(models.Model):
+    name = models.CharField(max_length=80)
+    teacher = models.ForeignKey(Teacher)
+    students = models.ManyToManyField(Student)
+    classes = models.ManyToManyField(Class)
+    speciality = models.ForeignKey(Speciality)
     college = models.ForeignKey(College)
-    descript = models.CharField(max_length=1000)
 
     def __unicode__(self):
-        return "teacher - ", self.user
+        return "Course: ".format(self.name)
+    
+class Classroom(models.Model):
+    name = models.CharField(max_length=50)
+    capacity = models.PositiveSmallIntegerField(default=0)
+    begintime = models.TimeField()  # time the classroom begin to use
+    endtime = models.TimeField()
+    status = models.CharField(max_length=30)  # 标识属于一天中的第几节课
+    courses = models.ManyToManyField(Course)
+
+    def __unicode__(self):
+        return "Classroom: ".format(self.name)
+    
+
