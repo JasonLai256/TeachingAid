0c1138377bcdb17d6c346bfd48f1e5476f46267b
Jason Lai <jasonlai256@gmail.com>
2012-03-21 22:20:04 +0800

    create the first view main_page and seccurefully running it.

--- taCore/views.py
+++ taCore/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+
+
+def main_page(request):
+    # shared_bookmarks = SharedBookmark.objects.order_by('-date')[:10]
+    # variables = RequestContext(request, {
+    #         'shared_bookmarks': shared_bookmarks
+    # })
+    return render_to_response('taCore/main.html')
+
+
